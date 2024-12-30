e77a062c991b2e537411404ce53d0ad83e36eb74
jeewantha <upuljeewantha070@gmail.com>
2024-12-30 11:06:17 +0530

    imlemented TRK for store

---
+++ src/store-V1.js
@@ -0,0 +1,16 @@
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import customerReducer from "./features/customers/customerSlice";
+import accountReducer from "./features/accounts/accountSlice";
+import { thunk } from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+
+const rootReducer = combineReducers({
+  account: accountReducer,
+  customer: customerReducer,
+});
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
+
+export default store;
--- src/store.js
+++ src/store.js
@@ -1,49 +1,12 @@
-import { createStore, combineReducers, applyMiddleware } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
 import customerReducer from "./features/customers/customerSlice";
 import accountReducer from "./features/accounts/accountSlice";
-import { thunk } from "redux-thunk";
-import { composeWithDevTools } from "redux-devtools-extension";
 
-const rootReducer = combineReducers({
-  account: accountReducer,
-  customer: customerReducer,
+const store = configureStore({
+  reducer: {
+    account: accountReducer,
+    customer: customerReducer,
+  },
 });
-const store = createStore(
-  rootReducer,
-  composeWithDevTools(applyMiddleware(thunk))
-);
 
 export default store;
-
-// // dispatching account actions
-// store.dispatch(deposit(5000));
-// console.log(store.getState());
-// store.dispatch(withdraw(1000));
-// console.log(store.getState());
-// store.dispatch(loanRequest(3000, "Buy a house"));
-// console.log(store.getState());
-// store.dispatch(payLoan());
-// console.log(store.getState());
-
-// // dispatching account actions
-// store.dispatch(createUser("Jeewantha", "970284044V"));
-// console.log(store.getState());
-// store.dispatch(updateUser("Nimantha"));
-// console.log(store.getState());
-
-// dispatching actions before creating action creators
-
-// store.dispatch({ type: "account/deposit", payload: 5000 });
-// store.dispatch();
-// console.log(store.getState());
-// store.dispatch({ type: "account/withdraw", payload: 1000 });
-// console.log(store.getState());
-// store.dispatch({
-//   type: "account/loanRequest",
-//   payload: { amount: 2000, pourpose: "Buy a car" },
-// });
-// console.log(store.getState());
-// store.dispatch({
-//   type: "account/payLoan",
-// });
-// console.log(store.getState());
